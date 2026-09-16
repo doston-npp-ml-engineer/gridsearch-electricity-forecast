8b8cdad564b845d525c803a8399e8a091b713a04
doston-npp-ml-engineer <pperceptron@gmail.com>
2026-09-16 16:26:58 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
 
 import streamlit as st
+
+st.set_page_config(page_title="Test", layout="wide")
+st.title("⚡ Test sahifa")
+st.write("Agar shu matn ko'rinsa, server ishlayapti, muammo modelda edi.")
